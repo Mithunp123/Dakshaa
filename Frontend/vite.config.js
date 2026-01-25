2026e54339325af0fe9c8a7af786d37cae5377d4
Mithun P <144042368+Mithunp123@users.noreply.github.com>
2026-01-25 17:26:33 +0530

    load app vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
   assetsInclude: ['**/*.heic'],
   server: {
     host: true,
-    allowedHosts: ['faec88edfa59.ngrok-free.app']
-  }
+    allowedHosts: ['235c0de834d2.ngrok-free.app']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      }
+    }
+  },
+  publicDir: 'public'
 })
